f03e31bd9cd1534f57c71b391e1e4280267eceae
carlaaotero <carla.otero@estudiantat.upc.edu>
2024-03-20 17:13:02 +0100

    Primera versió

---
+++ app/src/main/java/dsa/upc/edu/listapp/github/Repos.java
@@ -0,0 +1,22 @@
+package dsa.upc.edu.listapp.github;
+
+public class Repos {
+    public String name;
+    public String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
